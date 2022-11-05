9005852be4de99560ebbda450670cab723961c11
oussgoku <ouss.gharbii@gmail.com>
2022-11-05 22:16:03 +0100

    navbar fixed

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  done:boolean=false
+  loadScripts() {
 
+    // This array contains all the files/CDNs
+    const dynamicScripts = [
+      'assets/js/bootstrap.min.js',
+      "assets/js/jQuery.style.switcher.js",
+      "assets/js/custom.js",
+      "assets/js/jquery.min.js",
+      "assets/css/slick.js",
+      "assets/js/popper.min.js",
+      "assets/css/slider.js",
+      "assets/css/slider-bg.js",
+      "assets/css/smoothproducts.js",
+      "assets/css/snackbar.min.js",
+      "assets/css/plugins",
+      "assets/css/style.css",
+      "assets/js",
+    ];
+    for (let i = 0; i < dynamicScripts.length; i++) {
+      const node = document.createElement('script');
+      node.src = dynamicScripts[i];
+      node.type = 'text/javascript';
+      node.async = false;
+      document.getElementsByTagName('body')[0].appendChild(node);
+    }
+    this.done = true
+  }
   constructor() { }
 
   ngOnInit(): void {
+    this.loadScripts()
     
   }
 
